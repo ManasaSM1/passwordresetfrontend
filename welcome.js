30b72c9737e385bf33709fa4c4ec1f398f12ba8b
ManasaSM1 <89134868+ManasaSM1@users.noreply.github.com>
2022-01-27 14:05:45 +0530

    Add files via upload

---
+++ welcome.js
@@ -0,0 +1,29 @@
+let logoutUrl = "https://password-reset-api.herokuapp.com/logout";
+
+let loginUrl = "https://password-reset-api.herokuapp.com/login";
+
+
+(async () => {
+	const response = await fetch(loginUrl, { credentials: "include" });
+	let data = await response.json();
+
+	if (!data.loggedIn) {
+		location.href = "index.html";
+	}
+})();
+
+let form = document.querySelector("#logoutForm");
+
+form.addEventListener("submit", checkOnSubmit, true);
+
+async function checkOnSubmit() {
+	const response = await fetch(logoutUrl);
+
+	let data = await response.json();
+
+	if (data.loggedIn) {
+		location.href = "welcome.html";
+	} else {
+		location.href = "index.html";
+	}
+}
